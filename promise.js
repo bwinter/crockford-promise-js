db39db5e12664fda2e9b495422c53ea1baebd927
bwinter <brendan@adaptiv-consulting.com>
2013-05-12 19:09:04 -0700

    I have now added a really brief example of possible usage. As well as my comments on anticipated limitations.

--- promise.js
+++ promise.js
@@ -1,11 +1,31 @@
+//E.G.
+var promise = makePromise();
 
+function cheese(value){
+  value ++;
+}
+function milk(value){
+  value ++;
+}
+
+function doAll() {
+  longCalculation(function(value) { promise.fulfilled(value) });
+  promise.whenFulfilled(cheese);
+  promise.whenFulfilled(milk);
+}
+//Pit fall seems to be that doAll might continue even if the calculations arent done.
+//So the user can't gaurentee the promise has been fulfilled, thus won't know if they can
+// for example, draw a screen using results. Basically this is limited to interacting with
+// writes, but not reads.
+
+///
 function makePromise() {
   var status = 'unresolved'
     , outcome
     , waiting = []
     , dreading = [];
 
-  function vouch(deed, func) {
+  function on(deed, func) {
     switch (status) {
       case 'unresolved';
         (deed === 'fulfilled' ? waiting : dreading).push(func);
@@ -32,17 +52,17 @@ function makePromise() {
   }
 
   return {
-    when: function (func) {
-      vouch('fulfilled', func);
+    whenFulfilled: function (func) {
+      on('fulfilled', func);
     },
-    fail: function (func) {
-      vouch('smashed', func);
+    whenBroken: function (func) {
+      on('broken', func);
     },
-    fulfill: function (value) {
+    fulfilled: function (value) {
       resolve('fulfilled', value);
     },
-    smash: function (string) {
-      resolve('smashed', string);
+    broken: function (string) {
+      resolve('broken', string);
     },
     status: function () {
       return status;
